244f65419aac20c5b3d68c52b4a6ea211fe1a394
Manikandan S <ceitmanikandan24@gmail.com>
2026-02-18 23:29:33 +0530

    avg-greater-count

---
+++ day1.js
@@ -0,0 +1,17 @@
+// Given an array of positive integers, return the number of elements that are strictly greater than the average of all previous elements. Skip the first element.
+console.log(avgCount([100, 200, 350, 134, 453, 455, 100]))
+
+function avgCount(arr){
+    let count = 0;
+    let avg;
+    let sum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        avg = sum / i;
+        if (avg < arr[i]) count++;
+        sum += arr[i]
+        console.log(sum)
+    }
+    return (count)
+}
+
+
